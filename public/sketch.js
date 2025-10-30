00db1041728943c0eecb3125e4c10ea348a490d2
ayunahkim <79731952+ayunahkim@users.noreply.github.com>
2025-10-30 20:24:41 +0900

    add variables for some basic game mechanics

--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,23 @@
 let socket = io();
 let users = {};
 
+let usercount;
+
+//variables for student health bars
+let health = 100;
+let hunger = 100;
+let stress = 100;
+
+let healthprogress = document.getElementById("healthprogress");
+let hungerprogress = document.getElementById("hungerprogress");
+let stressprogress = document.getElementById("stressprogress");
+
+//default variables decrease by 1
+let decrease = 1;
+
+let gameover = false;
+
+
 function setup(){
     createCanvas(windowWidth,windowHeight);
     background(255);
@@ -22,4 +39,17 @@ function draw(){
     background(255);
     textAlign(CENTER);
     text("Placeholder :P",width/2,height/2);
+}
+
+function dying(){
+    //add conditions for number of clients active later
+    if(health<=0){
+        gameover = true;
+    }
+}
+
+function helpStudent(stat){
+    let temp = stat;
+    //buttons on click run this function
+    console.log("I ran! " + stat);
 }
